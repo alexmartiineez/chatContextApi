5d88849925e9b010551625f69614bd4c101625f2
Alexander Martinez <you@example.com>
2021-04-15 00:34:31 +0200

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Chat from './components/Chat'
+import { useContext } from 'react';
+import { chatContext } from './context/ChatProvider';
 
 function App() {
-  return (
+
+  const{user} = useContext(chatContext)
+
+  return user.state !== null ? (
     <div className="App">
+      <Navbar/>
+
+        {
+          user.state ? (
+            <Chat/>
+          ) : (
+            <div className='lead text-center mt-5'>
+              Debes iniciar sesion
+            </div>
+          )
+        }
+
       
     </div>
-  );
+  ) : (
+    <div>Cargando...</div>
+  )
 }
 
 export default App;
